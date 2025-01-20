c7c5ba06868ec48fc1f791ac20fc092764d607ae
curso_básico_Dio <marrimacelso@gmail.com>
2025-01-20 20:48:23 -0300

    first commit

---
+++ src/main/java/com/devmarrima/person_ptm/PersonPtmApplication.java
@@ -0,0 +1,13 @@
+package com.devmarrima.person_ptm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonPtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonPtmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmarrima/person_ptm/PersonPtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmarrima.person_ptm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonPtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
